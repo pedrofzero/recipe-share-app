f41d2a6e6ed33ff3736f216851ffae4f45f2255c
pedrofzero <pedrolf2000@gmail.com>
2023-07-09 13:45:14 +0000

    Imports

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,7 @@
+<script>
+	import Header from '../layout/header.svelte';
+</script>
+
+<Header />
 <h1 class="text-2xl">Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
