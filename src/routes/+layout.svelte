6c4cdf65d0574816622a1a73faab42429d9d1e00
pedrofzero <pedrolf2000@gmail.com>
2023-07-09 13:43:26 +0000

    Root page layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,4 +2,10 @@
 	import '../app.css';
 </script>
 
-<slot />
+<div class="h-screen w-screen">
+	<div class="h-full w-full max-w-3xl m-auto text-center">
+		<div class="text-start">
+			<slot />
+		</div>
+	</div>
+</div>
